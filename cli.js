8c6aaa7cf3950af422589660d4cc608d2ea5f3a3
Cat Tovey <cheestov@email.unc.edu>
2023-02-05 21:40:51 -0500

    added -h option

--- cli.js
+++ cli.js
@@ -1,1 +1,12 @@
 #!/usr/bin/env node
+
+const minimist = require('minimist');
+
+
+
+var args = minimist(process.argv.slice(2), {
+    boolean: "h",
+    default: {h: false}, 
+});
+
+console.log(args.h);
